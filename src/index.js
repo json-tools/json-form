40e338a28d5602eb6143058272552255b0f2b79d
Anatoliy <mail@anatoliy.in>
2018-09-24 22:44:29 +0100

    Published demo to gh pages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 
 require('../stylesheets/index.css');
 
+import registerServiceWorker from './registerServiceWorker';
+
+registerServiceWorker();
+
 const Elm = require('./Main');
 
 Elm.Elm.Main.init({ node: document.body });
